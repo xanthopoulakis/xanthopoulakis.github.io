3b17cf471f3041c559b1c7f773565d053b9408e4
C.Xanthopoulakis <c.xanthopoulakis@gmail.com>
2018-05-10 15:04:02 +0200

    synced with gGnome.js

---
+++ sync.sh
@@ -0,0 +1,6 @@
+cp -rpf ../gGnome.js/* ./
+find ./node_modules/ -name "*README.md" -type f -exec rm -rf {} \;
+cp ./index0.html ./index.html
+git add .
+git commit . -m 'synced with gGnome.js'
+git push origin master
