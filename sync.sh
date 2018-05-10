b9494f6e5c8ec2a06783025684564c6269aa741e
C.Xanthopoulakis <c.xanthopoulakis@gmail.com>
2018-05-10 15:07:11 +0200

    synced with gGnome.js

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 cp -rpf ../gGnome.js/* ./
-find ./node_modules/ -name "*README.md" -type f -exec rm -rf {} \;
+find ./node_modules/ -name "*.md" -type f -exec rm -rf {} \;
 cp ./index0.html ./index.html
 git add .
 git commit . -m 'synced with gGnome.js'
