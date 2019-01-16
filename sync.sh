80e128358e0182eaf73a90b5d3c288c0b494d3ce
C.Xanthopoulakis <c.xanthopoulakis@gmail.com>
2019-01-16 23:34:29 +0100

    synced with gGnome.js

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 cp -rpf ../gGnome.js/* ./
 find ./node_modules/ -name "*" -type f -exec rm -rf {} \;
 find ./ -name "package*" -type f -exec rm -rf {} \;
-cp ./index0.html ./index.html
+cp ./datafiles.csv0 ./datafiles.csv
 git add .
 git commit . -m 'synced with gGnome.js'
 git push origin master
