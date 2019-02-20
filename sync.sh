70f6cf069d88272c1d53a96a28f6ead96344239d
C.Xanthopoulakis <c.xanthopoulakis@gmail.com>
2019-02-20 17:52:21 +0100

    synced with gGnome.js

--- sync.sh
+++ sync.sh
@@ -1,5 +1,4 @@
 cp -rpf ../gGnome.js/* ./
-find ./node_modules/ -name "*" -type f -exec rm -rf {} \;
 rm -rf ./node_modules/*
 find ./ -name "package*" -type f -exec rm -rf {} \;
 cp ./datafiles.csv0 ./datafiles.csv
